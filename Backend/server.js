22d4e3d828d9afb699e05be4bf46a84eff3d28a0
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-05-03 16:34:16 +0530

    feat(ai): integrate Gemini AI for dynamic text generation

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,12 @@
 require("dotenv").config()
 const app =require("./src/app")
 const connectToDB=require("./src/config/db")
+const invokeGeminiAi= require("./src/services/ai.service")
+
+
+
 connectToDB()
+invokeGeminiAi()
 
 app.listen(3000,()=>{
     console.log("Server is Running on Port 3000")
